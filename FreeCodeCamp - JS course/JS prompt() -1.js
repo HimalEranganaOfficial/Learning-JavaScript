0e3c1f582dcf3a01454500c8a6eace4ab82fbc2b
Himal Erangana <62547849+HimalEranganaOfficial@users.noreply.github.com>
2025-03-06 16:49:55 +0530

    Create JS prompt() -1.js

---
+++ FreeCodeCamp - JS course/JS prompt() -1.js
@@ -0,0 +1,8 @@
+let day;
+day = "today";
+sentence = `Hi there...!,
+How can I help you ${day},
+`;
+let name = prompt("What is your name? ", "Himal Erangana");
+sentence = sentence.concat(name, "?")
+console.log(sentence)
