41dfc27496001f6d0fbd6bd1dddd572f7aea9251
Himal Erangana <62547849+HimalEranganaOfficial@users.noreply.github.com>
2025-03-08 22:42:57 +0530

    Create JS switch.js

---
+++ FreeCodeCamp - JS course/JS switch.js
@@ -0,0 +1,21 @@
+let ranNum = prompt("Enter a number to continue...!","5");
+ranNum = +ranNum;
+switch (ranNum) {
+case 1:
+console.log(`You have chosen no 1.`);
+break;
+case 2:
+console.log(`You have chosen no 2.`);
+break;
+case 3:
+console.log(`You have chosen no 3.`);
+break;
+case 4:
+console.log(`You have chosen no 4.`);
+break;
+case 5:
+console.log(`You have chosen no 5.`);
+break;
+default:
+console.log(`Something went wrong...!`);
+}
