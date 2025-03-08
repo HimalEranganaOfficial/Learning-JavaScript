ffcda9b605737392abdc4aff8a97be89cd5e6660
Himal Erangana <62547849+HimalEranganaOfficial@users.noreply.github.com>
2025-03-08 06:46:14 +0530

    Create Generate a Random Number.js

---
+++ FreeCodeCamp - JS course/Generate a Random Number.js
@@ -0,0 +1,6 @@
+//There are many Ways to do it.
+
+// Let us generate a number between 100 and 0.
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(`Your random number is ${randomNumber}`);
