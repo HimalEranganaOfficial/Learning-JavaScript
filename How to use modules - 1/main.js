29913926f44baa6986b1ab4c28a9434773422f5b
Himal Erangana <62547849+HimalEranganaOfficial@users.noreply.github.com>
2025-04-22 13:28:59 +0530

    Create main.js

---
+++ How to use modules - 1/main.js
@@ -0,0 +1,8 @@
+import {add, subtract, divide, multiply, PI} from "./math.js";
+
+console.log(add(10,2));
+console.log(subtract(10,2));
+console.log(multiply(10,2));
+console.log(divide(10,2));
+console.log(divide(2,10));
+console.log(100*PI);
